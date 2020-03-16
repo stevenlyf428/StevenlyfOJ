5c1127d58218960b03babaa096bbb9e03b1e0fed
stevenlyf428 <42695164+stevenlyf428@users.noreply.github.com>
2020-03-17 04:10:10 +0800

    222

--- python/testpython.py
+++ python/testpython.py
@@ -1,13 +1,20 @@
-# import opencv
-import numpy
-import pandas
-print("1111")
-print("222")
-a = 2
-print("第一次提交")
-import pandas
-import opencv
-print("第二次提交")
+# # import cv2
+# 删除一行：command+x
+#
+# import numpy
+# import pandas
+# print("1111")
+# print("222")
+# print("第一次提交")
+
+# cv2.imread()
+# print("第二次提交")
 
 
-print("第三次提交")
+# print("第三次提交")
+import numpy as np
+import cv2
+img = cv2.imread('1.jpg', cv2.IMREAD_UNCHANGED)
+cv2.namedWindow('img', cv2.WINDOW_AUTOSIZE)
+cv2.imshow('img', img)
+cv2.waitKey(0)
